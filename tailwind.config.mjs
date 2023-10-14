afb461d18edd5bc9535f2571cdc5a53373e4cc7f
Joshua <rodrigues.joshr@gmail.com>
2023-10-14 19:49:14 +0530

    skelle done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily:{
-				rammettoOne:'Rammetto One,Times New Roman, serif'
+				rammettoOne:'Rammetto One,Times New Roman, serif',
+				publicSans: 'Public Sans Variable, sans-serif'
 			}
 		},
 	},
