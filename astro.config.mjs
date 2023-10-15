9c52174c72d676347a3071b6aaf6856345ec3381
Joshua <rodrigues.joshr@gmail.com>
2023-10-15 23:03:14 +0530

    hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "hybrid",
+  adapter: vercel()
 });
---
+++ src/pages/api/mail.ts
@@ -0,0 +1,27 @@
+import type { APIRoute } from "astro"
+
+export const POST: APIRoute = async({ request }) => {
+  
+  const data = await request.formData();
+   const from = data.get("from");
+   const body = data.get("body");
+  
+  // // Validate the data - you'll probably want to do more than this
+   if (!from || !body) {
+     return new Response(
+       JSON.stringify({
+         message: "Missing required fields",
+       }),
+       { status: 400 }
+     );
+     
+   }
+  
+  // Do something with the data, then return a success response
+  return new Response(
+    JSON.stringify({
+      message: "Email sent"
+    }),
+    { status: 200 }
+  );
+  }
